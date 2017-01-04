e1710623313927d3046620b3e24d551d341617a6
abe33 <cedric.nehemie@gmail.com>
2017-01-04 13:39:17 +0100

    Add test setup to visually debug a test

--- spec/hover-manager-spec.js
+++ spec/hover-manager-spec.js
@@ -13,9 +13,35 @@ const {
 
 const projectPath = path.join(__dirname, 'fixtures');
 
-describe('HoverManager', () => {
+// By enabling this constant, it's possible to visually debug a test.
+// It should only be used when a single test is focused as it will make every
+// test last for one minute before completing.
+// During that time the atom's workspace will be visible in the test. After that
+// the normal test cleanup occurs and the workspace will be cleaned of all its
+// content.
+const VISUAL_DEBUG = false;
+let jasmineContent;
+
+fdescribe('HoverManager', () => {
   let editor;
   beforeEach(() => {
+    jasmineContent = !VISUAL_DEBUG
+      ? document.body.querySelector('#jasmine-content')
+      : document.body;
+
+    const styleNode = document.createElement('style');
+    styleNode.textContent = !VISUAL_DEBUG
+      ? ''
+      : `
+        atom-workspace {
+          z-index: 100000;
+          position: relative;
+        }
+      `;
+
+    jasmineContent.appendChild(styleNode);
+    jasmineContent.appendChild(atom.views.getView(atom.workspace));
+
     spyOn(metrics, 'track');
     jasmine.useRealClock();
     atom.config.set('kite.checkReadiness', true);
@@ -23,6 +49,14 @@ describe('HoverManager', () => {
     waitsForPromise(() => atom.workspace.open('sample.py').then(e => {
       editor = e;
     }));
+  });
+
+  afterEach(() => {
+    if (VISUAL_DEBUG) {
+      let done = false;
+      setTimeout(() => done = true, 59500);
+      waitsFor('nothing', 60000, () => done);
+    }
   });
 
   withKiteWhitelistedPaths([projectPath], () => {
