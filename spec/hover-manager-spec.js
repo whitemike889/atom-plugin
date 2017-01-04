c2336bdac19d0f1df89f73fc24fd952d3069648b
abe33 <cedric.nehemie@gmail.com>
2017-01-04 18:44:03 +0100

    Implement proper word detection and dismissing of overlays

--- spec/hover-manager-spec.js
+++ spec/hover-manager-spec.js
@@ -6,13 +6,13 @@ const http = require('http');
 const ready = require('../lib/ready.js');
 const metrics = require('../lib/metrics.js');
 const HoverManager = require('../lib/hover-manager');
-const {hoverPath} = require('../lib/utils');
+const {hoverPath, editorRoot} = require('../lib/utils');
 const {
   withKiteWhitelistedPaths, withRoutes, fakeResponse,
 } = require('./spec-helpers');
 
 const projectPath = path.join(__dirname, 'fixtures');
-const noShadowDOM = () => parseFloat(atom.getVersion()) >= 1.13;
+
 
 // By enabling this constant, it's possible to visually debug a test.
 // It should only be used when a single test is focused as it will make every
@@ -27,14 +27,10 @@ describe('HoverManager', () => {
   let editor, editorElement;
 
   const editorQuery = (selector) =>
-    noShadowDOM()
-      ? editorElement.querySelector(selector)
-      : editorElement.shadowRoot.querySelector(selector);
+    editorRoot(editorElement).querySelector(selector);
 
-  // const editorQueryAll = (selector) =>
-  //   noShadowDOM()
-  //     ? editorElement.querySelectorAll(selector)
-  //     : editorElement.shadowRoot.querySelectorAll(selector);
+  const editorQueryAll = (selector) =>
+    editorRoot(editorElement).querySelectorAll(selector);
 
   beforeEach(() => {
     jasmineContent = !VISUAL_DEBUG
@@ -79,14 +75,27 @@ describe('HoverManager', () => {
     });
 
     describe('.showHoverAtPosition()', () => {
-      it('triggers a request for the editor at the given position', () => {
-        HoverManager.showHoverAtPosition(editor, [2, 8]);
+      describe('when the position matches a word', () => {
+        it('triggers a request for the editor at the given position', () => {
+          HoverManager.showHoverAtPosition(editor, [2, 8]);
 
-        expect(http.request.mostRecentCall.args[0].path)
-        .toEqual(hoverPath(editor, [[2, 4], [2, 9]]));
+          expect(http.request.mostRecentCall.args[0].path)
+          .toEqual(hoverPath(editor, [[2, 4], [2, 9]]));
+        });
+      });
+
+      describe('when the position does not match a word', () => {
+        it('does not triggers a request', () => {
+          HoverManager.showHoverAtPosition(editor, [1, 0]);
+
+          expect(http.request.mostRecentCall.args[0].path)
+          .not.toEqual(hoverPath(editor, [[1, 0], [1, 0]]));
+        });
       });
 
       describe('when the position match the position of a token', () => {
+        let hover;
+
         withRoutes([
           [
             o => /^\/api\/buffer\/atom/.test(o.path),
@@ -97,12 +106,39 @@ describe('HoverManager', () => {
         beforeEach(() => {
           waitsForPromise(() =>
             HoverManager.showHoverAtPosition(editor, [2, 8]));
+          runs(() => hover = editorQuery('kite-hover'));
         });
 
         it('displays an overlay decoration with the results from the API', () => {
-          const hover = editorQuery('kite-hover');
           expect(hover).toExist();
           expect(hover.textContent.trim()).toEqual('hello');
+        });
+
+        describe('querying the same range again', () => {
+          beforeEach(() => {
+            waitsForPromise(() =>
+              HoverManager.showHoverAtPosition(editor, [2, 7]));
+          });
+
+          it('leaves the previous decoration in place', () => {
+            const newHover = editorQuery('kite-hover');
+            expect(newHover).toBe(hover);
+          });
+
+        });
+
+        describe('querying a different range', () => {
+          beforeEach(() => {
+            waitsForPromise(() =>
+              HoverManager.showHoverAtPosition(editor, [0, 1]));
+          });
+
+          it('destroys the previous decoration and creates a new one', () => {
+            expect(editorQueryAll('kite-hover').length).toEqual(1);
+
+            const newHover = editorQuery('kite-hover');
+            expect(newHover).not.toBe(hover);
+          });
         });
       });
 
