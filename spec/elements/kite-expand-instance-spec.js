6edcf41656077fbc3ca1e46a0f7b802ce439c05e
abe33 <cedric.nehemie@gmail.com>
2017-01-12 19:03:14 +0100

    Start using data for instance expand view

---
+++ spec/elements/kite-expand-instance-spec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const KiteExpandInstance = require('../../lib/elements/kite-expand-instance');
+
+describe('KiteExpandInstance', () => {
+  let json, element;
+
+  beforeEach(() => {
+    json = require('../fixtures/variable.json');
+    element = new KiteExpandInstance();
+    element.setData(json);
+  });
+
+  it('sets the name and type of the instance', () => {
+    expect(element.querySelector('.expand-header .name').textContent).toEqual('variable');
+    expect(element.querySelector('.expand-header .type').textContent).toEqual('int');
+  });
+
+  it('displays the synopsis in the view summary', () => {
+    expect(element.querySelector('.expand-body .summary').textContent).toEqual('symbol `variable`');
+  });
+});
