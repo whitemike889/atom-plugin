eb4bf03d2845d8a847fd25db17fd7035538d5d2d
abe33 <cedric.nehemie@gmail.com>
2017-01-19 17:52:35 +0100

    :construction: Start implementing notifications in NotificationsCenter

--- spec/notifications-center-spec.js
+++ spec/notifications-center-spec.js
@@ -41,41 +41,66 @@ describe('NotificationsCenter', () => {
     describe('and no notifications have been displayed before', () => {
       describe('when kite is not installed', () => {
         it('notifies the user', () => {
-          waitsForPromise(() => app.connect());
-          waitsFor(() =>
-            workspaceElement.querySelector('atom-notification'));
+          waitsForPromise(() => app.connect().then(() => {
+            const notificationElement = workspaceElement.querySelector('atom-notification');
+            const notification = notificationElement.getModel();
+            const options = notification.getOptions();
+
+            expect(notificationElement).toExist();
+
+            expect(notification.getType()).toEqual('warning');
+            expect(notification.getMessage())
+            .toEqual("Kite doesn't support your OS");
+
+            expect(options.buttons).toBeUndefined();
+            expect(options.dismissable).toBeTruthy();
+            expect(options.description)
+            .toEqual('Sorry, the Kite autocomplete engine only supports macOS at the moment.');
+          }));
         });
       });
 
       withKiteNotRunning(() => {
         it('notifies the user', () => {
-          waitsForPromise(() => app.connect());
-          waitsFor(() =>
-            workspaceElement.querySelector('atom-notification'));
+          waitsForPromise(() => app.connect().then(() => {
+            const notificationElement = workspaceElement.querySelector('atom-notification');
+            // const notification = notificationElement.getModel();
+
+            expect(notificationElement).toExist();
+          }));
         });
       });
 
       withKiteNotReachable(() => {
         it('notifies the user', () => {
-          waitsForPromise(() => app.connect());
-          waitsFor(() =>
-            workspaceElement.querySelector('atom-notification'));
+          waitsForPromise(() => app.connect().then(() => {
+            const notificationElement = workspaceElement.querySelector('atom-notification');
+            // const notification = notificationElement.getModel();
+
+            expect(notificationElement).toExist();
+          }));
         });
       });
 
       withKiteNotAuthenticated(() => {
         it('notifies the user', () => {
-          waitsForPromise(() => app.connect());
-          waitsFor(() =>
-            workspaceElement.querySelector('atom-notification'));
+          waitsForPromise(() => app.connect().then(() => {
+            const notificationElement = workspaceElement.querySelector('atom-notification');
+            // const notification = notificationElement.getModel();
+
+            expect(notificationElement).toExist();
+          }));
         });
       });
 
       withKiteWhitelistedPaths(() => {
-        it('does not notify the user', () => {
-          waitsForPromise(() => app.connect());
-          waitsFor(() =>
-            workspaceElement.querySelector('atom-notification'));
+        it('notifies the user', () => {
+          waitsForPromise(() => app.connect().then(() => {
+            const notificationElement = workspaceElement.querySelector('atom-notification');
+            // const notification = notificationElement.getModel();
+
+            expect(notificationElement).toExist();
+          }));
         });
       });
 
@@ -84,11 +109,69 @@ describe('NotificationsCenter', () => {
           atom.project.setPaths([__dirname]);
         });
 
-        it('notifies the user', () => {
+        it('does not notify the user', () => {
           waitsForPromise(() => app.connect().then(() => {
             expect(workspaceElement.querySelector('atom-notification')).not.toExist();
           }));
         });
+      });
+    });
+  });
+
+  describe('when there is no python file open', () => {
+    beforeEach(() => {
+      waitsForPromise(() => atom.workspace.open('variable.json'));
+    });
+
+    describe('when kite is not installed', () => {
+      it('does not notify the user', () => {
+        waitsForPromise(() => app.connect().then(() => {
+          expect(workspaceElement.querySelector('atom-notification')).not.toExist();
+        }));
+      });
+    });
+
+    withKiteNotRunning(() => {
+      it('does not notify the user', () => {
+        waitsForPromise(() => app.connect().then(() => {
+          expect(workspaceElement.querySelector('atom-notification')).not.toExist();
+        }));
+      });
+    });
+
+    withKiteNotReachable(() => {
+      it('does not notify the user', () => {
+        waitsForPromise(() => app.connect().then(() => {
+          expect(workspaceElement.querySelector('atom-notification')).not.toExist();
+        }));
+      });
+    });
+
+    withKiteNotAuthenticated(() => {
+      it('does not notify the user', () => {
+        waitsForPromise(() => app.connect().then(() => {
+          expect(workspaceElement.querySelector('atom-notification')).not.toExist();
+        }));
+      });
+    });
+
+    withKiteWhitelistedPaths(() => {
+      it('does not notify the user', () => {
+        waitsForPromise(() => app.connect().then(() => {
+          expect(workspaceElement.querySelector('atom-notification')).not.toExist();
+        }));
+      });
+    });
+
+    withKiteWhitelistedPaths([__dirname], () => {
+      beforeEach(() => {
+        atom.project.setPaths([__dirname]);
+      });
+
+      it('does not notify the user', () => {
+        waitsForPromise(() => app.connect().then(() => {
+          expect(workspaceElement.querySelector('atom-notification')).not.toExist();
+        }));
       });
     });
   });
