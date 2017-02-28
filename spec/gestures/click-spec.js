560e00591b9f9b2b7ed3d1740e7606dd4629f5dc
Cédric Néhémie <cedric.nehemie@gmail.com>
2017-02-28 13:14:09 +0100

    Feature hyperclick support (#81)
    * Add a keyboard-based gesture object
    
    * Refactor KiteEditor so that it handles gestures
    
    * Add test fixture for sample tokens
    
    * Extract tokens retrieval into a class
    
    * Refactor hover to check tokens before activate
    
    It also now handles a deactivate event
    
    * :green_heart: Fix members fixtures
    
    * Refactor click gesture to use a tokens list
    
    * Refactor cursor move gesture to use a tokens list
    
    * Refactor word selection gesture to use a tokens list
    
    * :bug: Fix finding token when a position is way past a line length
    
    * Use token as data in gesture to provide token's range
    
    * Rename arguments and fix linter
    
    * Update Keyboard gesture to support tokens list
    
    * :construction: WIP of hyperclick underline
    
    * Add hyperclick styles
    
    * Implement hyperclick goto definition
    
    * :fire: test focus

--- spec/gestures/click-spec.js
+++ spec/gestures/click-spec.js
@@ -1,47 +1,83 @@
 'use strict';
 
 const ClickGesture = require('../../lib/gestures/click');
+const TokensList = require('../../lib/tokens-list');
 const {click} = require('../helpers/events');
 
 describe('ClickGesture', () => {
-  let editor, editorElement, gesture, spy;
+  let editor, editorElement, gesture, activateSpy, deactivateSpy, tokensList, jasmineContent, workspaceElement;
 
   beforeEach(() => {
     jasmine.useRealClock();
+    jasmineContent = document.querySelector('#jasmine-content');
+    workspaceElement = atom.views.getView(atom.workspace);
+    jasmineContent.appendChild(workspaceElement);
+
     waitsForPromise(() => atom.packages.activatePackage('language-python'));
     waitsForPromise(() => atom.workspace.open('sample.py').then(e => {
       editor = e;
       editorElement = atom.views.getView(editor);
+      tokensList = new TokensList(editor, require('../fixtures/sample-tokens.json').tokens);
     }));
   });
 
   describe('without any modifier', () => {
     beforeEach(() => {
-      spy = jasmine.createSpy();
-      gesture = new ClickGesture(editor);
+      activateSpy = jasmine.createSpy();
+      deactivateSpy = jasmine.createSpy();
+      gesture = new ClickGesture(editor, tokensList);
 
-      gesture.onDidActivate(spy);
+      gesture.onDidActivate(activateSpy);
+      gesture.onDidDeactivate(deactivateSpy);
     });
 
     describe('when the mouse is clicked', () => {
-      beforeEach(() => {
-        click(editorElement, {x: 5, y: 5});
-      });
+      describe('on a token', () => {
+        beforeEach(() => {
+          click(editorElement, {x: 5, y: 5});
+        });
 
-      it('triggers a did-activate', (done) => {
-        expect(spy).toHaveBeenCalled();
+        it('triggers a did-activate event', (done) => {
+          expect(activateSpy).toHaveBeenCalled();
+        });
+
+        describe('and in the same token again', () => {
+          beforeEach(() => {
+            click(editorElement, {x: 10, y: 5});
+          });
+
+          it('does not trigger another did-activate event', (done) => {
+            expect(activateSpy.callCount).toEqual(1);
+          });
+        });
+
+        describe('to a place without token', () => {
+          beforeEach(() => {
+            click(editorElement, {x: 300, y: 5});
+          });
+
+          it('triggers a did-deactivate event', (done) => {
+            expect(deactivateSpy).toHaveBeenCalled();
+          });
+
+          it('does not trigger a new did-activate event', (done) => {
+            expect(activateSpy.callCount).toEqual(1);
+          });
+        });
       });
     });
   });
 
   describe('with modifiers', () => {
     beforeEach(() => {
-      spy = jasmine.createSpy();
-      gesture = new ClickGesture(editor, {
+      activateSpy = jasmine.createSpy();
+      deactivateSpy = jasmine.createSpy();
+      gesture = new ClickGesture(editor, tokensList, {
         altKey: true,
       });
 
-      gesture.onDidActivate(spy);
+      gesture.onDidActivate(activateSpy);
+      gesture.onDidDeactivate(deactivateSpy);
     });
 
     describe('when the mouse is clicked without modifier', () => {
@@ -50,7 +86,7 @@ describe('ClickGesture', () => {
       });
 
       it('does not trigger a did-activate event', (done) => {
-        expect(spy).not.toHaveBeenCalled();
+        expect(activateSpy).not.toHaveBeenCalled();
       });
     });
 
@@ -60,7 +96,7 @@ describe('ClickGesture', () => {
       });
 
       it('triggers a did-activate event', (done) => {
-        expect(spy).toHaveBeenCalled();
+        expect(activateSpy).toHaveBeenCalled();
       });
     });
   });
--- spec/gestures/cursor-move-spec.js
+++ spec/gestures/cursor-move-spec.js
@@ -1,31 +1,71 @@
 'use strict';
 
 const CursorMoveGesture = require('../../lib/gestures/cursor-move');
+const TokensList = require('../../lib/tokens-list');
 
 describe('CursorMoveGesture', () => {
-  let editor, gesture, spy;
+  let editor, gesture, activateSpy, deactivateSpy, tokensList, jasmineContent, workspaceElement;
 
   beforeEach(() => {
     jasmine.useRealClock();
+    jasmineContent = document.querySelector('#jasmine-content');
+    workspaceElement = atom.views.getView(atom.workspace);
+    jasmineContent.appendChild(workspaceElement);
+
     waitsForPromise(() => atom.packages.activatePackage('language-python'));
     waitsForPromise(() => atom.workspace.open('sample.py').then(e => {
       editor = e;
-      spy = jasmine.createSpy();
-      gesture = new CursorMoveGesture(editor);
-      gesture.onDidActivate(spy);
+      activateSpy = jasmine.createSpy();
+      deactivateSpy = jasmine.createSpy();
+      tokensList = new TokensList(editor, require('../fixtures/sample-tokens.json').tokens);
+      gesture = new CursorMoveGesture(editor, tokensList);
+
+      gesture.onDidActivate(activateSpy);
+      gesture.onDidDeactivate(deactivateSpy);
     }));
   });
 
-  beforeEach(() => {
-    editor.setCursorBufferPosition([0, 3]);
-    editor.setCursorBufferPosition([0, 4]);
-    editor.setCursorBufferPosition([0, 5]);
-    editor.setCursorBufferPosition([0, 6]);
-    editor.setCursorBufferPosition([0, 7]);
+  describe('when moved inside a token range', () => {
+    beforeEach(() => {
+      editor.setCursorBufferPosition([0, 3]);
+    });
+
+    it('triggers a did-activate event', () => {
+      expect(activateSpy).toHaveBeenCalled();
+    });
+
+    describe('and moved again inside the same range', () => {
+      beforeEach(() => {
+        editor.setCursorBufferPosition([0, 4]);
+      });
+
+      it('does not trigger another did-activate event', () => {
+        expect(activateSpy.callCount).toEqual(1);
+      });
+    });
+
+    describe('and moved outside a token', () => {
+      beforeEach(() => {
+        editor.setCursorBufferPosition([0, 13]);
+      });
+
+      it('triggers a did-deactivate event', () => {
+        expect(deactivateSpy).toHaveBeenCalled();
+      });
+
+      it('does not trigger another did-activate event', () => {
+        expect(activateSpy.callCount).toEqual(1);
+      });
+    });
   });
 
-  it('triggers a did-activate event', () => {
-    expect(spy).toHaveBeenCalled();
-    expect(spy.callCount).toEqual(5);
+  describe('moved at a place where there is no token', () => {
+    beforeEach(() => {
+      editor.setCursorBufferPosition([0, 13]);
+    });
+
+    it('does not trigger a did-activate event', () => {
+      expect(activateSpy).not.toHaveBeenCalled();
+    });
   });
 });
