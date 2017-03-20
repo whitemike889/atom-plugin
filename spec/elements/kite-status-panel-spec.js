65341a2f63577575915126846f64b2ef721c1c23
abe33 <cedric.nehemie@gmail.com>
2017-03-20 16:42:49 +0100

    :white_check_mark: Add tests for kite account status

--- spec/elements/kite-status-panel-spec.js
+++ spec/elements/kite-status-panel-spec.js
@@ -39,7 +39,7 @@ fdescribe('KiteStatusPanel', () => {
           const link = status.querySelector('.split-line .right a');
           expect(link).toExist();
           expect(link.textContent).toEqual('Account');
-          expect(link.href).toEqual('https://alpha.kite.com/pro');
+          expect(link.href).toEqual('http://localhost:46624/redirect/pro');
         }));
       });
     });
@@ -65,7 +65,80 @@ fdescribe('KiteStatusPanel', () => {
           const link = status.querySelector('.split-line .right a');
           expect(link).toExist();
           expect(link.textContent).toEqual('Upgrade');
-          expect(link.href).toEqual('https://alpha.kite.com/pro');
+          expect(link.href).toEqual('http://localhost:46624/redirect/pro');
+        }));
+      });
+    });
+
+    withPlan('trialing pro with less than 5 days remaining', {
+      status: 'trialing',
+      active_subscription: 'pro',
+      features: {},
+      trial_days_remaining: 3,
+      started_kite_pro_trial: true,
+    }, () => {
+      it('displays a pro badge with the remaining days in red text', () => {
+        waitsForPromise(() => status.show().then(() => {
+          expect(status.querySelector('.split-line .left .pro-badge')).toExist();
+          const days = status.querySelector('.split-line .left .kite-trial-days');
+          expect(days).toExist();
+          expect(days.textContent).toEqual('Trial: 3 days left');
+          expect(days.classList.contains('text-danger'));
+        }));
+      });
+
+      it('displays a link to upgrade to a pro account', () => {
+        waitsForPromise(() => status.show().then(() => {
+          const link = status.querySelector('.split-line .right a');
+          expect(link).toExist();
+          expect(link.textContent).toEqual('Upgrade');
+          expect(link.href).toEqual('http://localhost:46624/redirect/pro');
+        }));
+      });
+    });
+
+    withPlan('community that did not trialed Kite yet', {
+      status: 'active',
+      active_subscription: 'community',
+      features: {},
+      trial_days_remaining: 0,
+      started_kite_pro_trial: false,
+    }, () => {
+      it('displays as a kite basic account', () => {
+        waitsForPromise(() => status.show().then(() => {
+          expect(status.querySelector('.split-line .left').textContent).toEqual('kite_vector_icon\n Kite Basic');
+        }));
+      });
+
+      it('displays a link to start a trial', () => {
+        waitsForPromise(() => status.show().then(() => {
+          const link = status.querySelector('.split-line .right a');
+          expect(link).toExist();
+          expect(link.textContent).toEqual('Start Pro trial');
+          expect(link.href).toEqual('http://localhost:46624/redirect/trial');
+        }));
+      });
+    });
+
+    withPlan('community that already did the Kite trial', {
+      status: 'active',
+      active_subscription: 'community',
+      features: {},
+      trial_days_remaining: 0,
+      started_kite_pro_trial: true,
+    }, () => {
+      it('displays as a kite basic account', () => {
+        waitsForPromise(() => status.show().then(() => {
+          expect(status.querySelector('.split-line .left').textContent).toEqual('kite_vector_icon\n Kite Basic');
+        }));
+      });
+
+      it('displays a link to upgrade to a pro account', () => {
+        waitsForPromise(() => status.show().then(() => {
+          const link = status.querySelector('.split-line .right a');
+          expect(link).toExist();
+          expect(link.textContent).toEqual('Upgrade');
+          expect(link.href).toEqual('http://localhost:46624/redirect/pro');
         }));
       });
     });
