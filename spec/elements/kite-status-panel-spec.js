70567f777efcb5dccd3239d872ca3cc2585d6e3b
abe33 <cedric.nehemie@gmail.com>
2018-05-31 11:54:46 +0200

    :fire: debug statement in tests

--- spec/elements/kite-status-panel-spec.js
+++ spec/elements/kite-status-panel-spec.js
@@ -247,7 +247,7 @@ describe('KiteStatusPanel', () => {
     allEnterpriseInstallPaths: ['/path/to/app'],
   }, () => {
     beforeEach(() => {
-      waitsForPromise(() => status.show().catch(err => console.log(err)));
+      waitsForPromise(() => status.show());
     });
 
     it('does not display the account status', () => {
