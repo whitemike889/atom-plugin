461d29ab7b05c6f650ff9dea6adac00d997e8b0c
abe33 <cedric.nehemie@gmail.com>
2018-06-15 12:09:52 +0200

    Experiment with interleaving actions and expectations

--- spec/json-runner-spec.js
+++ spec/json-runner-spec.js
@@ -3,9 +3,23 @@
 const fs = require('fs');
 const path = require('path');
 const KiteAPI = require('kite-api');
-const {withKite} = require('kite-api/test/helpers/kite');
+const {withKite, withKitePaths} = require('kite-api/test/helpers/kite');
 
-const walk = (p, callback) => {
+fdescribe('JSON tests', () => {
+  beforeEach(() => {
+    jasmine.useRealClock();
+  });
+
+  walk(jsonPath('tests'), (testFile) => {
+    buildTest(require(testFile));
+  });
+});
+
+function jsonPath(p) {
+  return path.join(__dirname, 'json', p);
+}
+
+function walk(p, callback) {
   if (fs.existsSync(p)) {
     const stats = fs.lstatSync(p);
 
@@ -17,9 +31,63 @@ const walk = (p, callback) => {
       callback(p);
     }
   }
-};
+}
 
-const buildTest = (data) => {
+function buildAction(action) {
+  switch (action.action) {
+    case 'open':
+      waitsForPromise('file opened and focused', () => {
+        return atom.workspace.open(jsonPath(action.properties.file));
+      });
+      break;
+    case 'move-cursor':
+      runs(() => {
+        const editor = atom.workspace.getActiveTextEditor();
+        if (action.properties.offset) {
+          const pos = editor.getBuffer().positionForCharacterIndex(action.properties.offset);
+          editor.setCursorBufferPosition(pos);
+        }
+      });
+      break;
+  }
+}
+
+function buildExpectation(expectation) {
+  switch (expectation.expect) {
+    case 'request':
+      waitsFor(`request to '${expectation.properties.path}'`, function() {
+        return KiteAPI.request.mostRecentCall.args[0].path === expectation.properties.path &&
+        this.env.equals_(
+          JSON.parse(KiteAPI.request.mostRecentCall.args[1]),
+          loadResponseForEditor(
+            jsonPath(expectation.properties.body),
+            atom.workspace.getActiveTextEditor()));
+      });
+      break;
+  }
+}
+
+function loadResponseForEditor(p, e) {
+  const data = require(p);
+
+  for (const k in data) {
+    const v = data[k];
+    if (v === 'filled-out-by-testrunner') {
+      switch (k) {
+        case 'source':
+          data[k] = 'atom';
+          break;
+        case 'filename':
+          data[k] = e.getPath();
+          break;
+      }
+    }
+  }
+
+  return data;
+}
+
+function buildTest(data) {
   describe(data.description, () => {
     withKite({[data.setup.kited]: true}, () => {
       beforeEach(() => {
@@ -27,39 +95,21 @@ const buildTest = (data) => {
         atom.project.setPaths([path.resolve(__dirname, '..')]);
         waitsForPromise('kite activation', () => atom.packages.activatePackage('kite'));
       });
-
-      data.actions.forEach(action => {
-        switch (action.type) {
-          case 'focus':
-            beforeEach(() => {
-              waitsForPromise('file opened and focused', () => {
-                return atom.workspace.open(action.properties.file).then(editor => {
-                  if (action.properties.offset) {
-                    const pos = editor.getBuffer().positionForCharacterIndex(action.properties.offset);
-                    editor.setCursorBufferPosition(pos);
-                  }
-                });
-              });
-            });
-            break;
-        }
+      withKitePaths(data.setup, undefined, () => {
+        it('', () => {
+          data.runs.forEach(item => {
+            switch (item.type) {
+              case 'action':
+                buildAction(item);
+                break;
+              case 'expect':
+                buildExpectation(item);
+                break;
+            }
+          });
+        });
       });
 
-      data.expect.forEach(expectation => {
-        switch (expectation.type) {
-          case 'request':
-            it(`makes a ${expectation.properties.method} request to '${expectation.properties.path}'`, () => {
-              waitsFor('request', () => KiteAPI.request.calls.some(call => call.args[0].path === expectation.properties.path));
-            });
-            break;
-        }
-      });
     });
   });
-};
-
-fdescribe('JSON tests', () => {
-  walk(path.join(__dirname, 'json', 'tests'), (testFile) => {
-    buildTest(require(testFile));
-  });
-});
+}
